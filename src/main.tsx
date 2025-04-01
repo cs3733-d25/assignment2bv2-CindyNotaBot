abb2f159528f99abe839a20e49c6df5de42ef5ef
pntran8 <ptran.nup@gmail.com>
2025-04-01 00:37:10 -0400

    Updated main.tsx so it only calls App.tsx, and App.tsx has the page components

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {Hobby1} from './components/hobby1.tsx'
-import {Title} from './components/title.tsx'
 import {App} from './app'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <Title />
       <App />
-      <Hobby1 />
   </StrictMode>,
 )
