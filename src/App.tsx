71ce6076837fa8814739ccccdb94ceeda4f72050
Cindy Wu <xwucindy@gmail.com>
2025-04-03 13:42:46 -0400

    Fixed ui

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,25 @@ import './App.css'
 import ReactDOM from 'react-dom/client';
 import Title from './title.tsx';
 import Intro from './intro';
+import Hobby2 from "./hobby2.tsx";
 
+
+/*
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(<Title />);
 
 const introduction = ReactDOM.createRoot(document.getElementById('introduction')!);
 root.render(<Intro />);
-
+*/
 
 function App() {
 
     return (
-        <div> className = "Web-container"
+        <div className = "web-container">
             <Title/>
             <Intro Cindy="Cindy" Tran=" Tran's paragraph"/>
+            <Hobby2 />
+
         </div>
 
     );
